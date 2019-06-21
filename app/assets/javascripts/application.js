6483d2f2044a77d6d3b9f60876cdc0f43e0f23d9
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-06-21 19:20:39 +0000

    Add jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  $("a").click(function(event) {
+    alert("Jquery is working!");
+    event.preventDefault();
+  });
+});
